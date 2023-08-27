a5dce1526cc633589dd7f108e298761869576246
akaryan611 <102131606+akaryan611@users.noreply.github.com>
2023-08-28 01:50:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+
+
+
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    window.event.currentTarget.classList.add("active-link");
+    
+    document.getElementById(tabname).classList.add("active-tab");
+    
+}
+var sidemeu = document.getElementById("sidemenu");
+
+
+function openmenu(){
+sidemeu.style.right="0";
+}
+function closemenu(){
+    sidemeu.style.right="-200px";
+    }
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx3nUK48IrgfsRXrnJ5tjAEvx9P0XDoeGh5OpMf7tnfJ8S6koze2dp5u6kpQw-JCqcnNA/exec'
+const form = document.forms['submit-to-google-sheet'];
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert("successfully submit");
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
